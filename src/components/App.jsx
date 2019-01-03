3d704a9824132d81d3f3268b59cb19a2890cc989
Gaurav Chaturvedi <gaurav.pg007@gmail.com>
2019-01-04 00:54:14 +0530

    creation action 3

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react' ;
+import '../App.css';
+class App extends Component{
+    render(){
+        return(
+            <div className ="App">
+            <div className ="title"> Reminder Pro </div>
+            <div className ="form-inline">
+            <div className ="form-group">
+            <input
+            className="form-control"
+            placeholder="I have to..."
+            />
+            </div>
+            <button
+            type="button"
+            className ="btn btn-success"
+            >Add Task</button>
+            </div>
+            </div>
+        )
+    }
+}
+export default App;
